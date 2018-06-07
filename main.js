67008316f823809e3f6d6c0fb41df77aa129acea
mckelveygreg <mckelveygreg@gmail.com>
2018-06-06 22:21:04 -0700

    functional form

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+function checkBoxLimit() {
+    var checkBoxGroup = document.forms.surveyForm.favorite;
+    var limit = 3;
+
+    for (var i = 0; i < checkBoxGroup.length; i++) {
+        checkBoxGroup[i].onclick = function() {
+            var checkedcount = 0;
+            for (var j = 0; j < checkBoxGroup.length; j++) {
+                checkedcount += (checkBoxGroup[j].checked) ? 1 : 0;
+            }
+            if (checkedcount > limit) {
+                alert("You can only choose a maximum of " + limit + " favorites!");
+                this.checked = false;
+            }
+        }
+    }
+}
+checkBoxLimit();
